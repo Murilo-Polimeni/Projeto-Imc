da22dc381606f911d74662db8f9d1c217d6f51d1
Murilo-Polimeni <murilo.polimeni@hotmail.com>
2023-08-05 17:05:45 -0300

    inserindo cor na linha de acordo com o peso

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const ESTADO_MAGRO = "Abaixo do peso"
+const ESTADO_PESOIDEAL = "Peso ideal"
+const ESTADO_SOBREPESO = "SobrePeso"
+const ESTADO_OBESIDADE = "Obesidade"
+const ESTADO_OBESIDADEGRAVE = "Obesidade grave!"
+
 function calcularImc() {
   const sexo = document.querySelector("input[name='sexo']:checked")
 
@@ -15,40 +21,25 @@ function calcularImc() {
   }
 
   if (resultado < 18.5) {
-    estado = "A baixo do peso"
+    estado = ESTADO_MAGRO
     html += `Você está muito a baixo de seu peso certo, se alimente melhor`
   } else if (resultado >= 18.5 && resultado < 24.9) {
-    estado = "PesoIdeal"
+    estado = ESTADO_PESOIDEAL
     html += `Você está em seu peso normal, muito bem! continue cuidando de sua saude`
   } else if (resultado >= 25.0 && resultado < 29.9) {
-    estado = "SobrePeso"
+    estado = ESTADO_SOBREPESO
     html += `Você está com sobrepeso`
   } else if (resultado >= 30.0 && resultado < 39.9) {
-    estado = "Obesidade"
+    estado = ESTADO_OBESIDADE
     html += `Você está com obesidade`
   } else if (resultado >= 40.0) {
-    estado = "Obesidade grave!"
+    estado = ESTADO_OBESIDADEGRAVE
     html += `Você está com obesidade grave, cuidado !!`
   }
 
   paragrafo.innerText = html
   InserirLinha(nome, altura, peso, estado)
-  inserirNoLocalStorage(nome, altura, peso, estado) 
-}
-
-function InserirLinha(nome, altura, peso, estado) {
-  const tbody = document.querySelector("table.historico-tabela tbody")
-  tbody.insertAdjacentHTML(
-    "beforeend",
-    `
-  <tr>
-    <td>${nome}</td>
-    <td>${altura.toFixed(2).replace(".", ",")}</td>
-    <td>${peso}</td>
-    <td>${estado}</td>
-  </tr>
-  `
-  )
+  inserirNoLocalStorage(nome, altura, peso, estado)
 }
 
 function inserirNoLocalStorage(nome, altura, peso, estado) {
@@ -82,4 +73,45 @@ function limparCampos() {
   nome.value = ""
   altura.value = ""
   peso.value = ""
+}
+
+function InserirLinha(nome, altura, peso, estado) {
+  const tbody = document.querySelector("table.historico-tabela tbody")
+  const cor = getCorLinha(estado)
+  tbody.insertAdjacentHTML(
+    "beforeend",
+    `
+  <tr class="${cor}">
+    <td>${nome}</td>
+    <td>${altura.toFixed(2).replace(".", ",")}</td>
+    <td>${peso}</td>
+    <td>${estado}</td>
+  </tr>
+  `
+  )
+}
+
+// function getCorLinha(estado) {
+//   let cor = "azul"
+//   if (estado === ESTADO_MAGRO || estado === ESTADO_SOBREPESO) {
+//     cor = "amarelo"
+//   } else if (estado === ESTADO_OBESIDADE) {
+//     cor = "laranja"
+//   } else if (estado === ESTADO_OBESIDADEGRAVE) {
+//     cor = "vermelho"
+//   }
+//   return cor
+// }
+
+function getCorLinha(estado) {
+  if (estado === ESTADO_MAGRO || estado === ESTADO_SOBREPESO) {
+    return "amarelo"
+  }
+  if (estado === ESTADO_OBESIDADE) {
+    return "laranja"
+  }
+  if (estado === ESTADO_OBESIDADEGRAVE) {
+    return "vermelho"
+  }
+  return "azul"
 }
